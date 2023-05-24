376739b19416c760a2cf8633e227b1aa1d598526
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-24 16:30:31 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TEST <code>TEST</code>TEST
-        </p>
+        <p>TEST</p>
         <a
           className="App-link"
           href="https://reactjs.org"
